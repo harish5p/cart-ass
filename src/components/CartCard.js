4db51c33ef203c2d445572a3315e754027bc67b6
Harish Pentapalli <harish5p@users.noreply.github.com>
2023-03-03 05:08:51 +0000

    CartCard add CartItems

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -6,6 +6,9 @@ export const CartCard = () => {
     <main>
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
+      <span className="flex justify-center font-bold text-2xl m-4">
+        Cart Items: 2
+      </span>
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
           
           <tbody>
@@ -13,14 +16,14 @@ export const CartCard = () => {
               <td className="w-32 p-4 ">
                 <img src="/assets/images/1001.png" alt="Apple Watch"/>
               </td>
-              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+              <td className="px-12 py-4 font-semibold text-gray-900 dark:text-white">
                 Apple Watch
               </td>
               
-              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+              <td className="text-right px-6 py-4 font-semibold text-gray-900 dark:text-white">
                 $599
               </td>
-              <td className="px-6 py-4">
+              <td className="text-right px-6 py-4">
               <Link to="#" className="font-medium bg-red-600 dark:bg-red-500 hover:underline text-white dark:text-white">Remove</Link>
               </td>
             </tr>
@@ -28,14 +31,14 @@ export const CartCard = () => {
               <td className="w-32 p-4">
                 <img src="/assets/images/1002.png" alt="Apple Imac" />
               </td>
-              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+              <td className="px-12 py-4 font-semibold text-gray-900 dark:text-white">
                 Imac 27"
               </td>
              
-              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+              <td className="text-right px-6 py-4 font-semibold text-gray-900 dark:text-white">
                 $2499
               </td>
-              <td className="px-6 py-4">
+              <td className="text-right px-6  py-4">
               <Link to="#" className="font-medium bg-red-600 dark:bg-red-500 hover:underline text-white dark:text-white">Remove</Link>
               </td>
             </tr>
